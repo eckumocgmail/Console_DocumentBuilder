b4fa93389fa201149d456892ac70072b2621416c
Константин <eckumoc@gmail.com>
2022-09-04 06:17:03 +0300

    ╨б╨╛╨╖╨┤╨░╤О ╨╜╨╛╨▓╤Л╨╣ ╨┐╤А╨╛╨╡╨║╤В

---
+++ OpenDocument.cs
@@ -0,0 +1,234 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.DocumentBuilder
+{
+    public interface IElementFactory<TElement> where TElement : IElement
+    {
+        TElement Create(string type,
+            IDictionary<string, IParameter> properties,
+            IDictionary<string, Func<string, IDictionary<string, object>, object>> events);
+    }
+
+
+
+    /// <summary>
+    /// ///
+    /// </summary>
+    public interface IElement
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+    }
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TParameter"></typeparam>
+    public abstract class AbstractElement<TParameter> : IElement where TParameter : IParameter
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public IContainer<AbstractElement<TParameter>> Parent { get; set; }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TValidation"></typeparam>
+    public class XsdElement<TValidation> : AbstractElement<ValidationParameter>, IContainer<IValidation> where TValidation : IValidation
+    {
+        public TValidation Validator { get; set; }
+
+        public IValidation Append(IValidation element)
+        {
+            if( element.Validate(this.Name))
+            {
+                return element;
+            }
+            return null;
+        }
+
+        public IValidation Replace(IValidation element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Authentication: ValidationParameter, IValidation
+    {
+
+        private string _name;
+        public string GetName() => _name;
+        public string SetName(string name) => (_name= name);
+
+
+        public bool Validate(object value) => (new Random().NextDouble() < 0.5f);
+
+
+        public string GetSuccessMessage() => $"Hello {GetName()}";
+
+        public string GetFailedMessage() => $"No valid {GetName()}";
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ComplexType: XrdsContainer<Authentication>
+    {
+        public override void OnAppend(IContainer<XsdElement<Authentication>> container, XsdElement<Authentication> element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnReplace(IContainer<XsdElement<Authentication>> container, XsdElement<Authentication> element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public interface IContainer<TElement>
+    {
+        public TElement Append(TElement element);
+        public TElement Replace(TElement element);
+    }
+
+    
+
+    public abstract class AbstractContainer<TElement> : IContainer<TElement> where TElement : AbstractElement<ValidationParameter>
+    {
+        private List<TElement> elements = new List<TElement>();
+        public abstract void OnAppend(IContainer<TElement> container, TElement element);
+        public abstract void OnReplace(IContainer<TElement> container, TElement element);
+        public TElement Append(TElement element)
+        {
+            if (element.Parent != null)
+            {
+                element.Parent.Replace(element);
+            }
+            elements.Add(element);
+            OnAppend(this,element);
+            return element;
+        }
+
+        public TElement Replace(TElement element)
+        {
+
+            elements.Remove(element);
+            OnReplace(this, element);
+            return element;
+        }
+    }
+
+
+    public abstract class XrdsContainer<TValidation> : AbstractContainer<XsdElement<TValidation>> where TValidation : IValidation
+
+    {
+        protected IElementFactory<XsdElement<TValidation>> factory { get; set; }
+
+        public abstract override void OnAppend(IContainer<XsdElement<TValidation>> container, XsdElement<TValidation> element);
+        public abstract override void OnReplace(IContainer<XsdElement<TValidation>> container, XsdElement<TValidation> element);
+        
+    }
+
+
+
+    public class HeaderValidation: IValidation
+    {
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSuccessMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFailedMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class DiscoveryService: XsdElement<HeaderValidation>  
+    {
+    }
+
+  
+    public class XrdsDocument : XrdsContainer<Authentication>
+    {
+         
+
+        public Encoding Encoding => Encoding.ASCII;
+
+        public XrdsDocument() : base()
+        {
+
+        }
+
+
+
+        public override void OnAppend(IContainer<XsdElement<Authentication>> container, XsdElement<Authentication> element) {
+            var updated = 
+            this.Append(this.factory.Create("SomeAuth",
+                new Dictionary<string, IParameter>(),
+                new Dictionary<string, Func<string, IDictionary<string, object>, object>>()));          
+        }
+             
+
+        public override void OnReplace(IContainer<XsdElement<Authentication>> container, XsdElement<Authentication> element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
---
+++ ValidationParameter.cs
@@ -0,0 +1,116 @@
+﻿using Patterns.DocumentBuilder;
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Patterns.DocumentBuilder
+{
+
+    public interface IParameter
+    {
+        public T Get<T>(string name);
+        public T Set<T>(string name, T value);
+    }
+    public interface IValidation
+    {
+        public string GetName();
+        public string SetName(string name);
+        public bool Validate(object value);
+        public string GetSuccessMessage();
+        public string GetFailedMessage();
+    }
+    public abstract class AbstractValidation<TValidation> : IValidation, IContainer<TValidation>
+    {
+        public abstract string GetName();
+        public abstract string SetName(string name);
+        public abstract bool Validate(object context, object value);
+        public abstract TValidation Append(TValidation element);
+        public abstract TValidation Replace(TValidation element);
+        public abstract bool Validate(object value);
+        public abstract string GetSuccessMessage();
+        public abstract string GetFailedMessage();
+    }
+    public abstract class AbstractParameter<TValidation> : IParameter where TValidation : IValidation
+    {
+        private object _value { get; set; }
+        protected abstract IDictionary<string, IDictionary<string, object>> Validate();
+        public T Get<T>(string name) => (T)_value;
+        public T Set<T>(string name, T value) => (T)(_value = value);
+        public Func<object, object, bool> OnChanges = (prev, current) => {
+            return true;
+        };
+    }
+    public class ValidationParameter : AbstractParameter<AbstractValidation<IValidation>>, IContainer<IValidation> 
+    {
+        private IList<IValidation> Validations { get; set; } = new List<IValidation>();
+        public IDictionary<string, IValidation> AsDictionary() =>
+            new Dictionary<string, IValidation>(this.Validations.ToList().Select(val => new KeyValuePair<string, IValidation>(val.GetName(), val)));
+
+        public IValidation Append(IValidation element)
+        {
+            Validations.Add(element);
+            return element;
+        }
+
+        public IValidation Replace(IValidation element)
+        {
+            Validations.Remove(element);
+            return element;
+        }
+
+        protected override IDictionary<string, IDictionary<string, object>> Validate()
+        {
+            IDictionary<string, bool> results = new Dictionary<string, bool>();
+            IDictionary<string, string> messages = new Dictionary<string, string>();
+            object value = this.Get<object>("self");
+            foreach (var validation in Validations)
+            {
+                if ((results[validation.GetName()] = results.ContainsKey(validation.GetName())))
+                {
+                    throw new Exception("Validation results can be destroyed.");
+                }
+                if (validation.Validate(value))
+                {
+                    messages[validation.GetName()] = validation.GetSuccessMessage();
+                }
+            }
+            var boxedResults = new Dictionary<string, object>(results.Select(kv => new KeyValuePair<string, object>(kv.Key, (object)kv.Value)));
+            var boxedMessages = new Dictionary<string, object>(messages.Select(kv => new KeyValuePair<string, object>(kv.Key, (object)kv.Value)));
+            return new Dictionary<string, IDictionary<string, object>>()
+            {
+                { "results", boxedResults },
+                { "messages", boxedMessages },
+            };
+        }
+
+
+
+        public class RusWord : Attribute, IValidation  
+        {
+            public string GetName() => "";
+            public string SetName(string name) => "";            
+            public string GetSuccessMessage() => "";
+            public string GetFailedMessage() => "";
+            public bool Validate(object value) => false;
+        }
+
+        /// глагол
+        public class RusNoun : RusWord { 
+
+        }
+        /// глагол
+        public class RusAdjective : RusWord { }
+        
+
+        public static void Run(string[] args)
+        {
+            var parameter = new RusWord();
+            parameter.Append(new RusWord());
+        }
+    }
+}
